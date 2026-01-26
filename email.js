92b3f1e906b3b38de09d99a6bf308e9f63bc24e8
NMurphy85 <murphy.nathant@gmail.com>
2026-01-25 20:38:21 -0500

    added shapes to main page

--- email.js
+++ email.js
@@ -3,6 +3,19 @@
 //5pDuD2AqdU-n8mlGt
 let isModalOpen = false
 let contrastToggle = false;
+const scaleFactor = 1/20;
+
+function moveBackround(event){
+const shapes = document.querySelectorAll(".shape");
+const x = event.clientX * scaleFactor;
+const y = event.clientY * scaleFactor;
+for(let i=0; i<shapes.length; i++){
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    console.log(isOdd);
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`  
+}
+}
  function toggleContrast() {
     contrastToggle = !contrastToggle;
     if (contrastToggle) {
